66d1787971b62cd6c7a1b29f134a449e3c8a95c6
Buster "Silver Eagle" Neece <buster@busterneece.com>
2021-11-07 01:49:36 -0600

    Library commit.

---
+++ src/Attributes/DeepNormalize.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Azura\Normalizer\Attributes;
+
+use Attribute;
+
+#[Attribute(Attribute::TARGET_PROPERTY | Attribute::TARGET_METHOD)]
+class DeepNormalize
+{
+    private bool $deepNormalize;
+
+    public function __construct(bool $value)
+    {
+        $this->deepNormalize = $value;
+    }
+
+    public function getDeepNormalize(): bool
+    {
+        return $this->deepNormalize;
+    }
+}
---
+++ src/Exception/NoGetterAvailableException.php
@@ -0,0 +1,8 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Azura\Normalizer\Exception;
+
+class NoGetterAvailableException extends \Exception
+{}
